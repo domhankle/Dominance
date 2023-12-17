2dfd1ffe322e7b1d09a4d4f44c9d75372374e38b
Dom Hankle <domhankle03@gmail.com>
2023-12-17 13:10:29 -0500

    Learning about Windows.

--- src/dominance.cpp
+++ src/dominance.cpp
@@ -1,24 +1,23 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
+int main(){
+  
+  sf::Window window(sf::VideoMode(800, 800), "My First Window!", sf::Style::Default);
+  window.setVerticalSyncEnabled(true);  
+  while(window.isOpen())
+  {
+    
+    sf::Event event;
+    while(window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+      if(event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
     }
-
-    return 0;
+  }
+  
+  return 0;
 }
